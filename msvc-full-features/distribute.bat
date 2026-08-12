8a6d9cd5408068133c8e4805b0665d8e5cde8590
AndyScarlet233 <248809439+AndyScarlet233@users.noreply.github.com>
2026-08-12 08:34:36 +0800

    fix: preserve hidden mod files in Windows distributions

--- msvc-full-features/distribute.bat
+++ msvc-full-features/distribute.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir distribution
 xcopy dll\*.dll distribution\
-xcopy /s /e /i ..\data distribution\data
+xcopy /s /e /i /h ..\data distribution\data
 xcopy /s /e /i ..\config distribution\config
 xcopy /s /e /i ..\gfx distribution\gfx
 xcopy /s /e /i ..\lang\mo distribution\lang\mo
